f443e0d91690532f57d88924ce90cdf3c4899bff
Kuldeep Gade <gadekuldeep25@gmail.com>
2025-09-26 23:09:52 +0530

    login page set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,33 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import LandingPage from "./page/LandingPage.jsx"; 
+// import LoginPage from "./page/LoginPage.jsx";
+// import AdminPage from "./page/AdminPage.jsx";
+// import StudentDashboard from "./page/StudentPage.jsx";
+// import FacultyDashboard from "./page/FacultyDashboard.jsx";
+// import TimetableGenerator from "./page/TimetableGenerator.jsx";
+// import RegistrationPage from "./page/RegistrationPage.jsx";
+
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<LandingPage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/register" element={<RegistrationPage />} />
+//         <Route path="/admin/dashboard" element={<AdminPage />} />
+//         <Route path="/student/dashboard" element={<StudentDashboard />} />
+//         <Route path="/faculty/dashboard" element={<FacultyDashboard />}/>
+//         <Route path="/timetable/generate" element={<TimetableGenerator />}/>
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./page/LandingPage.jsx"; 
 import LoginPage from "./page/LoginPage.jsx";
 import AdminPage from "./page/AdminPage.jsx";
@@ -7,7 +35,6 @@ import StudentDashboard from "./page/StudentPage.jsx";
 import FacultyDashboard from "./page/FacultyDashboard.jsx";
 import TimetableGenerator from "./page/TimetableGenerator.jsx";
 import RegistrationPage from "./page/RegistrationPage.jsx";
-
 
 const App = () => {
   return (
